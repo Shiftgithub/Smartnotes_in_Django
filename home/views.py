91facf9270006eb256451b23c990fd930c3be55f
shiftgithub <mhturanmohammad@gmail.com>
2023-01-03 18:41:19 +0600

    Start

---
+++ home/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from datetime import datetime
+
+# def home(request):
+#     return HttpResponse('Assalamalaikum')
+
+
+def home(request):
+
+    return render(request, 'home/welcome.html', {'today': datetime.today()})
