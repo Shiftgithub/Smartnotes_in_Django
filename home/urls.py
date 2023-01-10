1ede068d5cbe31de8f146ea69958add940049a73
shiftgithub <mhturanmohammad@gmail.com>
2023-01-10 13:45:26 +0600

    chapter-4

--- home/urls.py
+++ home/urls.py
@@ -4,5 +4,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('home', views.home),
+    path('home', views.HomeView.as_view()),
+    path('authorized', views.AuthorizedView.as_view()),
 ]
