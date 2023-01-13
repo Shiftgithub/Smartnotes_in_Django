9c9136c76bbc527b950dc28c79c543182dd795f6
shiftgithub <mhturanmohammad@gmail.com>
2023-01-13 15:14:14 +0600

    updated

--- SmartNotes/notes/models.py
+++ SmartNotes/notes/models.py
@@ -6,3 +6,8 @@ class Notes(models.Model):
     text = models.TextField()
     created = models.DateTimeField(auto_now_add=True)
     user = models.ForeignKey(User,on_delete=models.CASCADE,related_name="notes")
+
+class Demos(models.Model):
+    title = models.CharField(max_length=250)
+    text = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
